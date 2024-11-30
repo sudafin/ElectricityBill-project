6a68c7aa071a859ef769913db2240dde925e6d40
huangyuhang <93380388+sudafin@users.noreply.github.com>
2024-11-30 14:45:18 +0800

    头像框样式更改

--- src/components/EBHeader.vue
+++ src/components/EBHeader.vue
@@ -1,19 +1,23 @@
 <template>
   <el-header class="eb-header">
     <div class="eb-header-left">
-      <img src="@/assets/images/logo.png" alt="logo" class="eb-logo">
+      <img src="@/assets/images/logo.png"
+           alt="logo"
+           class="eb-logo">
     </div>
     <div class="eb-header-right">
+      <div class="eb-user-details">
+        <span class="eb-user-name">{{ adminInfo.username }} | {{ adminInfo.role }}</span>
+      </div>
       <el-dropdown>
         <div class="eb-user-info">
-          <el-avatar :size="42" :src="adminInfo.avatar"></el-avatar>
-          <div class="eb-user-details">
-            <span class="eb-user-name">{{ adminInfo.username }} | {{ adminInfo.role }}</span>
-          </div>
+          <el-avatar :size="42"
+                     :src="adminInfo.avatar"></el-avatar>
         </div>
         <template #dropdown>
           <el-dropdown-menu>
-            <el-dropdown-item divided @click="handleLogout">退出登录</el-dropdown-item>
+            <el-dropdown-item divided
+                              @click="handleLogout">退出登录</el-dropdown-item>
           </el-dropdown-menu>
         </template>
       </el-dropdown>
@@ -22,27 +26,27 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
-import { useUserStore } from '@/store/user';
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { useUserStore } from '@/store/user'
 
-const router = useRouter();
-const userStore = useUserStore();
+const router = useRouter()
+const userStore = useUserStore()
 // 使用假数据替换用户名和角色
 const adminInfo = ref({
   username: userStore.adminInfo.userName,
   role: userStore.adminInfo.roleName,
   avatar: 'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png',
-});
+})
 
 const handleLogout = async () => {
   try {
-    await userStore.logout();
-    router.push('/login');
+    await userStore.logout()
+    router.push('/login')
   } catch (error) {
-    console.error('退出登录失败:', error);
+    console.error('退出登录失败:', error)
   }
-};
+}
 </script>
 
 <style scoped>
@@ -65,17 +69,16 @@ const handleLogout = async () => {
   display: flex;
   align-items: center;
   cursor: pointer;
-  padding: 10px;
+  padding: 5px;
   border-radius: 4px;
   transition: background-color 0.3s;
 }
-
 
 .eb-user-details {
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-left: 20px;
+  margin-right: 10px;
 }
 
 .eb-user-name {
